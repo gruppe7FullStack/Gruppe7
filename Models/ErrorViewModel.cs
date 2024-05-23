69906010ad0060705a164a0ca2573f319ba5a83a
Gruppe7 <gruppe7fullstack@gmail.com>
2024-05-23 21:17:01 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gruppe7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
