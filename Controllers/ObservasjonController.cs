e6ac8bb1f1328e6d1862e2319c3065e71974a8f6
Gruppe7 <gruppe7fullstack@gmail.com>
2024-05-24 00:30:15 +0200

    Del 2: deloppgave 7a
    <Lag ny side for temperatur målinger fra FrostAPI>
    
    - Laget ny side ved bruk av scaffolding teknikken.
    
    - Slettet unødvendige filer som Create.cshtml, Delete.cshtml osv. samt deres kode i kontrolleren som ble automatisk generert under scaffoldingen.
    
    Deloppgave 7a er ferdig

--- Controllers/ObservasjonController.cs
+++ Controllers/ObservasjonController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gruppe7.Data;
+using Gruppe7.Models;
 
 namespace Gruppe7.Controllers
 {
     public class ObservasjonController : Controller
     {
-        public IActionResult Index()
+        private readonly DatabaseContext _context;
+
+        public ObservasjonController(DatabaseContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: Observasjon
+        public async Task<IActionResult> Index()
+        {
+            var observations = await _context.Observation
+                .OrderByDescending(o => o.Date)
+                .Take(7) // Kun de 7 siste dagene
+                .ToListAsync(); ; // Dette var nødvendig fordi i oppgave teksten står det at kontroller og katalogen i views skal ha forskjellige navn
+            return View("~/Views/Observation/Index.cshtml", observations); // localhost:<port>/Observasjon
         }
     }
 }
+
