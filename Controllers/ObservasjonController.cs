ecdcc8016c634644971769ad9b9c56acc5bce5e3
Gruppe7 <gruppe7fullstack@gmail.com>
2024-05-24 00:07:42 +0200

    Del 2
    Implementasjon av Del2 har startet.

---
+++ Controllers/ObservasjonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe7.Controllers
+{
+    public class ObservasjonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
